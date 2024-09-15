f3266467baba14b8a2c3d133666b08390d10695e
Reithan <bo122081@hotmail.com>
2024-09-15 10:03:59 -0700

    add miss text with player setting

---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "fad-display-misses",
+        order = "ab",
+        setting_type = "runtime-per-user",
+        default_value = true
+    }
+})
